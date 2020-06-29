28bc53c177f86ee157ad13276ab6849e40a78e68
Thunnathorne <atom9583@gmail.com>
2020-06-29 11:56:28 -0500

    font

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,21 +1,35 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    
+    <HelloWorld msg="Welcome to Atom Vue.js App"/> 
+    <center>Hello </center>
+    <i class="fab fa-facebook-square"></i>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+  export default {
   name: 'App',
   components: {
     HelloWorld
   }
 }
 </script>
+
+// <script>
+// import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
+// import Vue from 'vue'
+// import Vuetify from 'vuetify/lib'
+
+// Vue.use(Vuetify)
+
+// export default new Vuetify({
+//   icons: {
+//     iconfont: 'fa',
+//   },
+// })
+// </script>
 
 <style>
 #app {
--- vueapp/src/components/HelloWorld.vue
+++ vueapp/src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <h1>{{ msg }}</h1>
-    <i class="fab fa-facebook-square"></i>
-    <p>
+    <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -27,7 +27,7 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul>-->
   </div>
 </template>
 
